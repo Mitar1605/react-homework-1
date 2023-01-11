93c3e9733a0174f2c00618ff7105dc2cef16516c
Mitar1605 <120776088+Mitar1605@users.noreply.github.com>
2023-01-11 16:38:19 +0400

    added section end light mode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [isDark, setIsDark] = useState(false)
+
+    const setDark = function(){
+        setIsDark(!isDark)
+    }
+
   return (
-    <div className="App">
+        <div className={isDark ? "bgr-light": "bgr-dark"}>
       <header>
         <div className="header__container">
           <div className="header__logo-div">
             <img src={require("./icons/fishing.png")} alt="hook"/>
-            <span>
+            <span className={isDark ? "color-dark" : "color-light"}>
                 Hook
               </span>
           </div>
           <div className="header_nav">
             <nav>
               <ul className="header_ul">
+                  <li className="header_ul-li"><img onClick={setDark} src={isDark ? require("./icons/moon.png"): require("./icons/sun.png")} alt=""/></li>
                 <li className="header_ul-li"><a href="#" className="header__ul-li-a">Home</a></li>
                 <li className="header_ul-li"><a href="#" className="header__ul-li-a">Features</a></li>
                 <li className="header_ul-li"><a href="#" className="header__ul-li-a">Pricing</a></li>
                 <li className="header_ul-li"><a href="#" className="header__ul-li-a">Blog</a></li>
                 <li className="header_ul-li"><a href="#" className="header__ul-li-a">
-                          <span>
+                          <span className={isDark ? "btn-light" : "btn-dark"}>
                             Register
                           </span>
                 </a></li>
@@ -29,6 +36,35 @@ function App() {
           </div>
         </div>
       </header>
+
+      <section>
+        <div className="sect__container">
+            <div className="join-div">
+                <a href="#">
+                    Join meet Up
+                </a>
+                <span>
+                    We are coming Singapore Learn more...
+                </span>
+            </div>
+            <div className="sect-cotnent">
+                <span className={isDark ? "color-dark" : "color-light"}>
+                    Serve Your Website fast
+                </span>
+
+                <div className="form-div">
+                    <input type="email" placeholder="Email Address"/>
+                    <button>
+                        Get Started
+                    </button>
+                </div>
+
+                <p className={isDark ? "color-dark" : "color-light"}>
+                    No credit card required
+                </p>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
